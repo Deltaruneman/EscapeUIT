9a2c71ac71a73c4906597e1d6df27ddde9d53d78
Deltaruneman <hogiabao3110@gmail.com>
2026-05-05 20:48:51 +0700

    Update main.js

--- main.js
+++ main.js
@@ -656,7 +656,7 @@ window.startBossBattle = function() {
 
     showBattleMenu(false);
     typeDialog("* ...Mày nghĩ lấy đủ 4 chìa khóa là thoát được sao?").then(() => {
-        return typeDialog("* Kẻ kém cỏi như ngươi chỉ đến được tới đây thôi.");
+        return new Promise(r => setTimeout(r, 800));
     }).then(() => {
         return typeDialog("* TA là UIT. Ta trường tồn. Và ngươi... sẽ ở lại đây MÃI MÃI.");
     }).then(() => {
